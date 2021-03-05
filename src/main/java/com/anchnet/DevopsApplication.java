0b3f5ca193fe2c0327f695d3b0861e1c73b6401e
lizy <lizy@anchnet.com>
2021-03-05 13:51:00 +0800

    devops demo test

---
+++ src/main/java/com/anchnet/DevopsApplication.java
@@ -0,0 +1,11 @@
+package com.anchnet;
+
+/**
+ * @author lzy
+ * @date 2021/3/5 13:46
+ */
+public class DevopsApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
